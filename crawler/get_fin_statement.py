59aec2001220c671b440b896e056f5e4f5cd2ec2
tranthanhbinh1 <tranngocthanhbinh01@gmail.com>
2023-09-16 13:45:32 +0700

    small changes + files

--- crawler/get_fin_statement.py
+++ crawler/get_fin_statement.py
@@ -5,10 +5,11 @@ import pandas as pd
 from utils.s3_connector import df_to_s3_parquet
 
 StatementType = typing.Literal["BalanceSheet", "IncomeStatement", "CashFlow"]
+PageType = typing.Literal[0, 8, 16]
 
 
 class GetFinancialStatement:
-    def __init__(self, organ_code: str, statement_type: StatementType):
+    def __init__(self, organ_code: str, statement_type: StatementType, page: PageType):
         self.organ_code = organ_code
         self.statement_type = statement_type
         self.page = 0
@@ -18,46 +19,52 @@ class GetFinancialStatement:
         url = f"https://fiin-fundamental.ssi.com.vn/FinancialStatement/Download{self.statement_type}?language=en&OrganCode={self.organ_code}&Skip={self.page}&Frequency=Quarterly"
 
         headers = {
-            'authority': 'fiin-fundamental.ssi.com.vn',
-            'accept': '*/*',
-            'accept-language': 'en-US,en;q=0.9,vi;q=0.8',
-            'access-control-request-headers': 'content-type,x-fiin-key,x-fiin-seed,x-fiin-user-id',
-            'access-control-request-method': 'GET',
-            'origin': 'https://iboard.ssi.com.vn',
-            'referer': 'https://iboard.ssi.com.vn/',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-site',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.76',
-            'Cookie': '__cf_bm=DTCnVdL.ZYR5Qb68BHPnX_vbWURBrOKVBPNboZGMkwQ-1694345120-0-AXeE6idhxc1Z0SHIK7m50wA6Nc13DQfj+gW62Oi5omSL7qpP1NWjh5cmjnVCpO4FDK/DWbsOeSIt1/TQ3BRWCEI='
+            "authority": "fiin-fundamental.ssi.com.vn",
+            "accept": "*/*",
+            "accept-language": "en-US,en;q=0.9,vi;q=0.8",
+            "access-control-request-headers": "content-type,x-fiin-key,x-fiin-seed,x-fiin-user-id",
+            "access-control-request-method": "GET",
+            "origin": "https://iboard.ssi.com.vn",
+            "referer": "https://iboard.ssi.com.vn/",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-site",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.76",
+            "Cookie": "__cf_bm=DTCnVdL.ZYR5Qb68BHPnX_vbWURBrOKVBPNboZGMkwQ-1694345120-0-AXeE6idhxc1Z0SHIK7m50wA6Nc13DQfj+gW62Oi5omSL7qpP1NWjh5cmjnVCpO4FDK/DWbsOeSIt1/TQ3BRWCEI=",
         }
 
         response = requests.request("GET", url, headers=headers)
 
-        with io.BytesIO(response.content) as buffer:
-            df = pd.read_excel(buffer, engine="openpyxl")
+        if response.status_code == 200:
+            with open(
+                f"financial_statements/{self.statement_type}_{self.organ_code}_{self.page}.xlsx", "wb"
+            ) as f:
+                f.write(response.content)
 
-            # Reset and drop Indexes
-            df = df.iloc[6:]
-            df = df.reset_index(drop=True)
-            # Tranpose and reset the index again
-            df = df.transpose().reset_index(drop=True)
-            # Set the first row as headers
-            new_headers = df.iloc[0]
-            df = df[1:]
-            df.columns = new_headers
-            df = df.rename(columns={"ITEMS": "Quarter"})
+        # with io.BytesIO(response.content) as buffer:
+        #     df = pd.read_excel(buffer, engine="openpyxl")
 
-            # df = df.astype(str)
-            df.columns = df.columns.astype(str)
+        #     # Reset and drop Indexes
+        #     df = df.iloc[6:]
+        #     df = df.reset_index(drop=True)
+        #     # Tranpose and reset the index again
+        #     df = df.transpose().reset_index(drop=True)
+        #     # Set the first row as headers
+        #     new_headers = df.iloc[0]
+        #     df = df[1:]
+        #     df.columns = new_headers
+        #     df = df.rename(columns={"ITEMS": "Quarter"})
 
-            self.df = df  # Typecast all to string to -> parquet
+        #     # df = df.astype(str)
+        #     df.columns = df.columns.astype(str)
 
+        #     self.df = df  # Typecast all to string to -> parquet
+        #     self.df.to_parquet(f"financial_statements/{self.statement_type}_{self.organ_code}.parquet")
         # Transform DF to Parquet and upload to S3
         # TODO: add from_year and to_year
-        df_to_s3_parquet(df=self.df, bucket_name="fiinpro-api",
-                         file_name=f"FinancialStatement/{self.statement_type}_{self.organ_code}")
+        # df_to_s3_parquet(df=self.df, bucket_name="fiinpro-api",
+        #                  file_name=f"FinancialStatement/{self.statement_type}_{self.organ_code}")
 
 
 # if __name__ == "__main__":
-    # run_fin_statement = GetFinancialStatement("SSI", "BalanceSheet") #Test
+# run_fin_statement = GetFinancialStatement("SSI", "BalanceSheet") #Test
--- crawler/main.py
+++ crawler/main.py
@@ -7,11 +7,13 @@ from .get_vn100 import get_vn100_symbols
 def main():
     vn100 = get_vn100_symbols()
     fin_stm_type = ["BalanceSheet", "IncomeStatement", "CashFlow"]
+    fin_page = [0, 8, 16]
 
     for symbol in vn100:
         for type in fin_stm_type:
-            get_fin_instance = GetFinancialStatement(
-                organ_code=symbol, statement_type=type)
+            for page in fin_page:
+                get_fin_instance = GetFinancialStatement(
+                    organ_code=symbol, statement_type=type, page=page)
             
 
 if __name__ == "__main__":
