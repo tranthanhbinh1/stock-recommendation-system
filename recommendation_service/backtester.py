3fd2312cf6c1e7d0b6106d95c63affd066f837fa
tranthanhbinh1 <tranngocthanhbinh01@gmail.com>
2024-01-07 15:37:29 +0700

    update

--- recommendation_service/backtester.py
+++ recommendation_service/backtester.py
@@ -1,50 +1,65 @@
 import pandas as pd
 import logging
+from backtesting import Backtest, Strategy
+from backtesting.lib import crossover
 from .technical_analysis import TechnicalAnalysis
 from utils.timescale_connector import TimescaleConnector
 from config.logging_config import setup_logging
+from backtesting import Backtest, Strategy
 
 
 setup_logging()
-class Backtester:
-    def __init__(self, symbol):
+
+
+class MACrossover(Strategy):
+    def init(self):
+        self.ma5 = self.I(lambda x: x, self.data.MA5)
+        self.ma20 = self.I(lambda x: x, self.data.MA20)
+
+    def next(self):
+        if not self.position and crossover(self.ma5, self.ma20):
+            self.buy()
+        elif self.position and crossover(self.ma20, self.ma5):
+            self.position.close()
+
+
+# bt = Backtest(df_PDR, Backtester, cash=100000000, commission=0.003)
+
+# output = bt.run()
+
+
+class Backtester(Backtest):
+    def __init__(self, symbol: str):
         self.symbol = symbol
-        self.df = TimescaleConnector.query_ohlcv_daily(symbol=self.symbol)
-        self.buy_signals = []
-        self.sell_signals = []
+        self.technical = TechnicalAnalysis(
+            df_technical=TimescaleConnector.query_ohlcv_daily(self.symbol)
+        )
+        self.data = (
+            pd.DataFrame()
+            .assign(
+                Open=self.technical.df_technical.open,
+                High=self.technical.df_technical.high,
+                Low=self.technical.df_technical.low,
+                Close=self.technical.df_technical.close,
+                Volume=self.technical.df_technical.volume,
+                MA5=self.technical.df_technical.MA5,
+                MA20=self.technical.df_technical.MA20,
+            )
+            .astype(float)
+            .set_index(self.technical.df_technical.index)
+            .dropna()
+        )
+        # self.data.to_csv("data.csv")
 
-    def calculate_ma(self):
-        analyzer = TechnicalAnalysis(df_technical=self.df)
-        self.df = analyzer.calculate_ma()
+    def backtest(self):
+        logging.info(self.data.info())
+        bt = Backtest(self.data, MACrossover, cash=1000000000, commission=0.003)
+        output = bt.run()
+        logging.info(output)
+        return output
 
-    def trading_strategy(self):
-        self.buy_signals = []
-        self.sell_signals = []
-
-        for i in range(1, len(self.df)):
-            if (
-                self.df["MA5"].iloc[i] > self.df["MA20"].iloc[i]
-                and self.df["MA5"].iloc[i - 1] < self.df["MA20"].iloc[i - 1]
-            ):
-                self.buy_signals.append(self.df.index[i])
-            elif (
-                self.df["MA5"].iloc[i] < self.df["MA20"].iloc[i]
-                and self.df["MA5"].iloc[i - 1] > self.df["MA20"].iloc[i - 1]
-            ):
-                self.sell_signals.append(self.df.index[i])
-
-    def trading_signals(self):
-        logging.info("Buy Signals:")
-        buy_signals_df = pd.DataFrame(self.buy_signals, columns=["Signal"])
-        logging.info(buy_signals_df)
-
-        logging.info("\nSell Signals:")
-        sell_signals_df = pd.DataFrame(self.sell_signals, columns=["Signal"])
-        logging.info(sell_signals_df)
 
 # Usage:
 if __name__ == "__main__":
     backtester = Backtester(symbol="SSI")
-    backtester.calculate_ma()
-    backtester.trading_strategy()
-    backtester.trading_signals()
+    backtester.backtest()
