5b81a0406c40785e2259b659dae364cfff5a8089
tranthanhbinh1 <tranngocthanhbinh01@gmail.com>
2025-01-04 23:39:46 +0700

    start with interfaces for crawler and db

---
+++ infrastructure/db/database.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+class Database(ABC):    #TODO: implement a Singleton pattern
+    @abstractmethod
+    def connect(self):
+        pass
+
+    @abstractmethod
+    def query(self, query: str):
+        pass
+    
+    @abstractmethod
+    def insert(self, df, table_name, schema, exist_strat):
+        pass
+    
+    @abstractmethod
+    def close(self):
+        pass
---
+++ interface/crawler.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+
+
+class Crawler(ABC):
+    @abstractmethod
+    def crawl(self, url: str) -> str:
+        pass
+    
+    
