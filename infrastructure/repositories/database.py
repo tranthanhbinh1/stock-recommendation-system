ad59692bee194f897e478afd96ca305f14204edb
tranthanhbinh1 <tranngocthanhbinh01@gmail.com>
2025-01-05 15:16:21 +0700

    update

---
+++ infrastructure/repositories/database.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+
+
+class Database(ABC):
+    """Provide interface for database operations."""
+
+    # TODO: Write SQL scripts to create tables, and implement database ops here.
+    pass
